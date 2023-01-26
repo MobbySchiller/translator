9a0f457eb171e688013060cd9dc9d25469bb15b9
MobbySchiller <kb.burzynski@gmail.com>
2023-01-26 09:28:08 +0100

    feat: add app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,28 @@
 import { FC } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-import { theme } from '../lib/styles'
+import { theme } from '../lib/styles/theme'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import TranslatorScreen from '../components/TranslatorScreen'
 
 export const App: FC = () => {
 
     return (
         <ThemeProvider theme={theme}>
-            <div>
-                App
-            </div>
+            <AppContainer>
+                <Header />
+                <TranslatorScreen />
+                <Footer />
+            </AppContainer>
         </ThemeProvider>
     )
 }
 
 const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     width: 100%;
     height: 100vh;
-    backgroundColor: ${({ theme }) => theme.colors.background}
+    background-color: ${({ theme }) => theme.colors.background}
 `
